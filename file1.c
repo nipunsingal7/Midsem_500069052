447ce1b758b4cda526ec1d6d985d298efea71b2f
nipunsingal <500069052@stu.upes.ac.in>
2019-03-18 12:53:39 +0530

    Q3 second commit

--- file1.c
+++ file1.c
@@ -11,15 +11,15 @@ int main()
     scanf("%d%d", &num1, &num2);
 
 
-    sum = num1 + num2;
+    
     sub = num1 - num2;
     mult=num1*num2;
-    div=num1/num2;
+    
 
-    printf("SUM = %d\n", sum);
+    
     printf("DIFFERENCE = %d\n", sub);
     printf("multy=%d",mult);
-    printf("divid=%d",div);
+    
     return 0;
 }
 
